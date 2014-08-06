6188f1c10e9d62f7bdfe96dded48ffa7bb5e1c18
Tony Richards <trichards@indiezen.com>
2014-08-06 12:25:46 -0400

    Added main function to service registry so it'll run as a standalone service.

--- src/zen/fabric/service_registry/service.py
+++ src/zen/fabric/service_registry/service.py
@@ -1,4 +1,6 @@
 from zen.fabric.service import Service
+from zen.fabric import service_registry
+from zen.fabric.batch_service_container import BatchServiceContainer
 
 class ServiceRegistry(Service):
     ''' Service Registry
@@ -24,3 +26,29 @@ class ServiceRegistry(Service):
     def put(self, path, addresses):
         self._services[path] = addresses
         return {'status' : 'ok'}
+
+# Main function that run this service in a standalone process
+def main():
+    import argparse
+    # Get arguments
+    arg_parser = argparse.ArgumentParser(description='Run the Data Registry')
+    arg_parser.add_argument('--port', default=8888, help='Service Registry port')
+
+    args = arg_parser.parse_args()
+
+    # Run Main
+    # Create service container and service(s)
+    container = BatchServiceContainer()
+    container.init(request_port=args.port)
+
+    registry = ServiceRegistry()
+
+    # The service registry is always only a local service because it shouldn't 
+    # be registered with another service registry (at least not until there are
+    # redundant service registries)
+    container.register_service(registry, service_registry.PATH, True)
+
+    container.run()
+
+if __name__ == "__main__":
+    main()
